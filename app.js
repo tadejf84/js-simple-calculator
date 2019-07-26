a2249726fb6961a1c34cba51602fd536b976347d
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-07-26 19:32:14 +0200

    calculator logic done

--- app.js
+++ app.js
@@ -21,18 +21,20 @@ class Calculator {
         this.curOperand = '';
         this.prevOperand = '';
         this.operation = '';
+        CalculatorUI.updateDisplay();
     }
 
 
     delete() {
-
+        this.curOperand = this.curOperand.toString().slice(0, -1);
+        CalculatorUI.updateDisplay(this.prevOperand, this.curOperand, this.operation);
     }
 
     // append number/separator to current operand
     appendNumber(num) {
         if(num === '.' && this.curOperand.includes('.')) return; // only one decimal point allowed
         this.curOperand = this.curOperand + num.toString();
-        CalculatorUI.updateDisplay(this.prevOperand, this.curOperand);
+        CalculatorUI.updateDisplay(this.prevOperand, this.curOperand, this.operation);
     }
 
     // selected operation
@@ -42,19 +44,65 @@ class Calculator {
         this.operation = operation;
         this.prevOperand = this.curOperand;
         this.curOperand = '';
-        CalculatorUI.updateDisplay(this.prevOperand, this.curOperand);
+        CalculatorUI.updateDisplay(this.prevOperand, this.curOperand, this.operation);
     }
 
     compute() {
-        console.log('computing...');
+        let result;
+        const prevVal = +this.prevOperand;
+        const curVal = +this.curOperand;
+        switch (this.operation) {
+            case '+': 
+                result = prevVal + curVal;
+                break;
+            case '-':
+                result = prevVal - curVal;
+                break;
+            case '/':
+                result = prevVal / curVal;
+                break;
+            case '*':
+                result = prevVal * curVal;
+                break;
+            default:
+                return;
+        }
+        this.curOperand = result;
+        this.prevOperand = '';
+        this.operation = '';
+        CalculatorUI.updateDisplay(this.prevOperand, this.curOperand, this.operation);
     }
 }
 
 // calculator class for UI
 class CalculatorUI {
-    static updateDisplay(prevOperand, curOperand) {
-        prevOperandDisplay.innerText = prevOperand;
-        curOperandDisplay.innerText = curOperand;
+
+    // get numbers in correct format
+    static formatNumber(num) {
+        const stringNum = num.toString();
+        const integerDigits = +(stringNum.split('.')[0]);
+        const decimalDigits = stringNum.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+          integerDisplay = '';
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+        }
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`;
+        } else {
+          return integerDisplay;
+        }
+    }
+
+    // updat display
+    static updateDisplay(prevOperand = '', curOperand = '', operation = '') {
+        curOperandDisplay.innerText = this.formatNumber(curOperand);
+        if(operation !== '') {
+            prevOperandDisplay.innerText = `${this.formatNumber(prevOperand)} ${operation}`;
+        } else {
+            prevOperandDisplay.innerText = '';
+        }
     }
 }
 
@@ -76,4 +124,16 @@ btnsOperation.forEach( btn => {
     btn.addEventListener('click', () => {
         calculator.operationSelect(btn.innerText);
     });
+});
+
+btnEquals.addEventListener('click', btn => {
+    calculator.compute();
+});
+
+btnClear.addEventListener('click', btn => {
+    calculator.clear();
+});
+
+btnDel.addEventListener('click', btn => {
+    calculator.delete();
 });
